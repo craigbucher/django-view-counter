25c14c91133f0c88e2878b1bb5dbe19595714926
“Craig <97258442+craigbucher@users.noreply.github.com>
2022-07-11 14:39:44 -0500

    working?

--- counter_app/views.py
+++ counter_app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import random
 
-# Create your views here.
+users = {}
+
+def index(request):
+    # check for exsiting cookie/session
+    user_id = request.COOKIES.get('user_id')
+    user = users.get(user_id)
+
+    # if no cookie, generate a random value for the user_id
+    if not user: 
+        user_id = str(random.randint(10000000, 99999999))
+
+        users[user_id] = {
+            # set the count to 1
+            'count': 1
+        }
+    else:
+        # if the user has an existing session, increment the count by 1
+        users[user_id]['count'] += 1
+
+    #return the response, setting the cookie for the user)id
+    response = render(request, 'counter_app/index.html', users[user_id])
+    response.set_cookie('user_id', user_id)
+    return response
